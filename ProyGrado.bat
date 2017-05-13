dbddc490b084095d743fc66e30e9f6127d17a2a0
Damian Salvia <damian.salvia@gmail.com>
2017-05-13 12:24:08 -0300

    adding more corpus

--- ProyGrado.bat
+++
@@ -1,5 +0,0 @@
-:: Levantar command-line
-:: docker-machine restart default
-docker-machine env default
-docker start proyecto_contenedor /bin/bash || docker run -d -t -i -v /c/Github/ProyGrado:/ProyGrado/ --name proyecto_contenedor proyecto_image /bin/bash
-docker exec -it proyecto_contenedor bash
