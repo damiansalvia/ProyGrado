a50d1c4bb4cf16eae1a4c16d15c55bb119ac390e
NicoMechu <nicomebu@gmail.com>
2017-05-23 17:54:51 -0300

    changes in lexicon_generator & lexicon_intesector

--- apps/independent_system/lexicon_generator.py
+++ apps/independent_system/lexicon_generator.py
@@ -6,59 +6,72 @@ import codecs
 import json
 import glob
 from collections import defaultdict
-from apps.utilities.printHelper import *
-
+from printHelper import *
 
 inputdir  = 'outputs/corpus/'
 outputdir = 'outputs/lexicons/single_corpus_lexicon/'
 logdir    = '../log/'
-
-def get_tokens(review):
-    return nltk.word_tokenize(review)
-
-def add_word_to_dic(occurrences, word, rank):
-    if not occurrences.get(word):
-        occurrences[word] = []
-    occurrences[word].append(rank)
-
-def get_polarity(list):
-    val = sum(list)/len(list)
-    if val < 40:
-        return '-'
-    elif val > 60:
-        return '+'
-    else:
-        return '0'
-
-# Main
 log = Log(logdir)
-start_time = time.time()
 
-files = glob.glob(inputdir + 'corpus*.json')
 
-for file in files:
-    file_name = file.split('/')[-1]
-    occurrences = defaultdict(list)
-    with codecs.open(file, "r", "utf-8") as f:
-        reviews = json.load(f)
-    corpus_length = len(reviews)
-    for idx, rev in enumerate(reviews):
-        progressive_bar( 'Processing ' + file_name.replace('.json', '').replace('_', ' ') + " : ", corpus_length, idx)
-        try:
-            review  = rev['review']
-            rank  = rev['rank']
-            for token in get_tokens(review):
-                occurrences[token.lower()].append(rank) 
-        except Exception as e:
-            log(str(e))
-            raise e
-    progressive_bar( 'Processing ' + file_name.replace('.json', '').replace('_', ' ') + " : ", corpus_length, idx+1)
-    print
-    polarities = {key:get_polarity(value) for key, value in occurrences.iteritems()}
-    print outputdir 
-    print file_name.replace('corpus', 'polarities')
-    with codecs.open(outputdir + file_name.replace('corpus', 'polarities'), "w", "utf-8") as f:
-        json.dump(polarities, f,indent=4,sort_keys=True,ensure_ascii=False)
+class IndependentLexiconGenerator:
+    
 
-print '\nElapsed time: %.2f Sec' % (time.time() - start_time)
+    def __init__(self, input_dir = inputdir):
+        self.files = glob.glob(input_dir + 'corpus*.json')
+        self.polarities = {}
+    
 
+    def generate(self):
+
+        def get_tokens(review):
+            return nltk.word_tokenize(review)
+
+        def get_polarity(list):
+            val = sum(list)/len(list)
+            if val < 40:
+                return '-'
+            elif val > 60:
+                return '+'
+            else:
+                return '0'
+
+        for file in self.files:
+            file_name = file.split('/')[-1]
+            occurrences = defaultdict(list)
+            with codecs.open(file, "r", "utf-8") as f:
+                reviews = json.load(f)
+            corpus_length = len(reviews)
+            for idx, rev in enumerate(reviews):
+                progressive_bar( 'Processing ' + file_name.replace('.json', '').replace('_', ' ') + " : ", corpus_length, idx)
+                try:
+                    review  = rev['review']
+                    rank  = rev['rank']
+                    tokens = get_tokens(review)
+                    for token in tokens:
+                        occurrences[token.lower()].append(rank) 
+                except Exception as e:
+                    log(str(e))
+                    raise e
+            progressive_bar( 'Processing ' + file_name.replace('.json', '').replace('_', ' ') + " : ", corpus_length, idx+1)
+            print
+        self.polarities[file_name] = {key:get_polarity(value) for key, value in occurrences.iteritems()}
+        
+    def get_polarities(self):
+        return self.polarities
+
+    def save(self, output_dir = outputdir):
+        for (file_name, pol) in self.polarities.iteritems():
+            with codecs.open(output_dir + file_name.replace('corpus', 'polarities'), "w", "utf-8") as f:
+                json.dump(pol, f,indent=4,sort_keys=True,ensure_ascii=False)
+
+
+if __name__ == "__main__":
+
+    start_time = time.time()
+
+    generator = IndependentLexiconGenerator()
+    generator.generate()
+    generator.save()
+
+    print '\nElapsed time: %.2f Sec' % (time.time() - start_time)
--- apps/independent_system/lexicon_intersector.py
+++ apps/independent_system/lexicon_intersector.py
@@ -10,36 +10,56 @@ from utils import *
 
 inputdir  = 'outputs/lexicons/single_corpus_lexicon/'
 outputdir = 'outputs/lexicons/'
+
 logdir    = '../log/'
+log = Log(logdir)
 
 MIN_MATCHES = 5
 
-def all_equal(list):
-    return all(map(lambda x: x == list[0], list))
+class IndependentLexiconIntersector:
+   
 
-# Main
-files = glob.glob(inputdir + 'polarities*.json')
-log = Log(logdir)
-start_time = time.time()
-polarities = defaultdict(list)
-for file in files:
-    try:
-        file_name = file.split('/')[-1]
-        with codecs.open(file, "r", "utf-8") as f:
-            corpus_polarities = json.load(f)
-        words_count = len(corpus_polarities)
-        for idx, (word,polarity) in enumerate(corpus_polarities.iteritems()):
-            progressive_bar( 'Processing ' + file_name.replace('.json', '').replace('_', ' ') + " : ", words_count, idx)
-            polarities[word].append(polarity)
-    except Exception as e:
-        log(str(e))
-        raise e
-    progressive_bar( 'Processing ' + file_name.replace('.json', '').replace('_', ' ') + " : ", words_count, idx+1)
-    print 
-    
-polarities = {word:polarities[word][0] for word in polarities if  len(polarities[word]) >= MIN_MATCHES and all_equal(polarities[word])} 
+    def __init__(self, input_dir = inputdir):
+        self.files = glob.glob(input_dir + 'polarities*.json')
+        self.polarities = defaultdict(list)
 
-with codecs.open(outputdir + 'static_polarities.json' , "w", "utf-8") as f:
-    json.dump(polarities, f, indent=4, ensure_ascii=False)
+    def intersect_corpus(self):
+        
+        def all_equal(list):
+            return all(map(lambda x: x == list[0], list))
 
-print '\nElapsed time: %.2f Sec' % (time.time() - start_time)
+        for file in self.files:
+            try:
+                file_name = file.split('/')[-1]
+                with codecs.open(file, "r", "utf-8") as f:
+                    corpus_polarities = json.load(f)
+                words_count = len(corpus_polarities)
+                for idx, (word,polarity) in enumerate(corpus_polarities.iteritems()):
+                    progressive_bar( 'Processing ' + file_name.replace('.json', '').replace('_', ' ') + " : ", words_count, idx)
+                    self.polarities[word].append(polarity)
+            except Exception as e:
+                log(str(e))
+                raise e
+            progressive_bar( 'Processing ' + file_name.replace('.json', '').replace('_', ' ') + " : ", words_count, idx+1)
+            print 
+        self.lexicon = {word:self.polarities[word][0] 
+            for word in self.polarities if  len(self.polarities[word]) >= MIN_MATCHES and all_equal(self.polarities[word])} 
+
+
+    def save(self, output_dir):
+        with codecs.open(output_dir + 'static_polarities.json' , "w", "utf-8") as f:
+            json.dump(self.lexicon, f, indent=4, ensure_ascii=False)
+
+
+
+#=====================================================================================================
+if __name__ == "__main__":
+
+    start_time = time.time()
+
+    intersector = IndependentLexiconIntersector()
+    intersector.intersect_corpus()
+    intersector.save()
+
+    print '\nElapsed time: %.2f Sec' % (time.time() - start_time)
+
