4fa934a1e06576e2cd05549d7fdcdf72d5510471
Damian Salvia <damian.salvia@gmail.com>
2017-06-13 22:00:37 -0300

    bug fix

--- apps/independent_system/negation_anotation.py
+++ apps/independent_system/negation_anotation.py
@@ -3,7 +3,7 @@ from corpus_reader import CorpusReader
 import os, random, json, io
 
 tmp = os.popen('stty size', 'r').read().split()
-width = int(tmp[1])-15 if tmp else 100
+WIDTH = int(tmp[1])-15 if tmp else 100
 
 # source extension review_pattern category_pattern category_location category_position category_level start decoding
 parameters = [
@@ -16,14 +16,14 @@ parameters = [
 ]
  
 def DisplayMenu():
-    print "************************ MENU ************************"
-    print "0. SALIR"
-    print "1. APPS" 
-    print "2. CINE"
-    print "3. HOTELES"
-    print "4. PRENSA"
-    print "5. TWEETS"
-    print "6. SFU"
+    print "*"*20," MENU ","*"*20
+    print "0. EXIT"
+    print "1. Corpus APPS" 
+    print "2. Corpus CINE"
+    print "3. Corpus HOTELES"
+    print "4. Corpus PRENSA"
+    print "5. Corpus TWEETS"
+    print "6. Corpus SFU"
     return 
  
 def DisplayStatus(idx,total,words,cats):
@@ -33,87 +33,122 @@ def DisplayStatus(idx,total,words,cats):
         elif i > idx:
             print words[i],
         else:
-            print '\033[92m'+words[i]+"\033[0m",
+            print "\033[92m\033[4m"+words[i]+"\033[0m\033[0m",
     print
- 
-while True:
-    DisplayMenu()
-    op = raw_input("> ")
+    
+def chunkstring(string, length):
+    return (string[0+i:length+i] for i in range(0, len(string), length))
+
+def DisplayAnnotated(result,width=WIDTH):
+    template = "%-7s | %-"+str(WIDTH-10)+"s"
+    print template % ("ID","ANNOTATION")
+    line = "%-7s-+-%-"+str(WIDTH-10)+"s"
+    print line % ("-"*7,"-"*100)
+    for item in result:
+        chunks = chunkstring(item['annotation'], width)
+        chunks = list(chunks)
+        print template % (str(item['id']),chunks[0])
+        for chunk in chunks[1:]:
+            print template % ("",chunk)
+
+def ViewSave(result):
     os.system('clear')
-    if len(op)>1 or op not in "0123456":
-        print "Opcion invalida"
-        continue
-    op = int(op)
-    if op == 0:
-        # Salir
-        break
-    else:
-        # Read the parameters
-        parameter = parameters[op-1]
-        name = parameters[op-1][0].split("/")[-1]
-        corpus = CorpusReader(
-            parameter[0],
-            parameter[1],
-            parameter[2],
-            parameter[3],
-            parameter[4],
-            category_position=parameter[5],
-            category_level=parameter[6],
-            start=parameter[7],
-            decoding=parameter[8],
-        )
-        # Ask how manu review for annotating
-        op = ""
-        while not op.isdigit():
-            op = raw_input("How many? > ")
-        left = int(op)
-        # Get the reviews an shuffle them for pick random reviews
-        reviews = corpus.get_opinions()
-        random.shuffle(reviews)
-        result = []
-        for review in reviews:
-            words = review.split(' ')
-            total = len(words)
-            cats = ['' for _ in range(total)]
-            idx = 0
-            # For each word annotate with N or I and give the positibility of back by pressing B
-            while idx != total:
-                os.system('clear') 
-                DisplayStatus(idx,total,words,cats)
-                cat = raw_input("N(ormal), I(nverted) or B(ack)? > ")
-                cat = cat.upper()
-                if cat not in 'NIBnib':
-                    print "Option",cat,"is not correct." ;raw_input()
-                    continue
-                if cat == 'B':
-                    # BACK
-                    idx = idx - 1 if idx != 0 else 0
-                    cats[idx] = ''
-                else:
-                    # Associate the category
-                    cats[idx] = cat
-                    idx = idx + 1
-            # Save the result as two list: words and its respective category for each one 
-            result.append({
-                "id" : left,
-                "annotation" : ' '.join(word+"/"+cat for word,cat in zip(words,cats))
-#                 'id'         : left,
-#                 'words'      : words,
-#                 'categories' : cats  
-            })
-            # Check finish
-            left -= 1
-            if left == 0:
-                print "DONE";raw_input()
-                break
-            print "Result added. Are you ready for the next? (left %i)" % left
-        # Save the result
-        odir = "./outputs/negation"
-        if not os.path.isdir(odir): 
-            os.makedirs(odir)
-        odir = odir+"/negated_%s.json" % name
-        with io.open(odir,"w",encoding='utf8') as f:
-            content = json.dumps(result,indent=4,ensure_ascii=False)
-            f.write(content)    
-                    
+    op = raw_input("Done! View result? (y/n) > ")
+    # Ask for display
+    if op.lower() == 'y':
+        DisplayAnnotated(result)
+    # Ask for save twice 
+    op = raw_input("\nSave result? (y/n) > ")
+    if op.lower() == 'n':
+        op = raw_input("Are you sure? (y/n) > ")
+        if op.lower() == 'y':
+            return
+    # Save the result
+    odir = "./outputs/negation"
+    if not os.path.isdir(odir): 
+        os.makedirs(odir)
+    odir = odir+"/negated_%s.json" % name
+    with io.open(odir,"w",encoding='utf8') as f:
+        content = json.dumps(result,indent=4,ensure_ascii=False)
+        f.write(content) 
+
+def Main():
+    result = []     
+    while True:
+        # Display menu options
+        os.system('clear')
+        DisplayMenu()
+        op = raw_input("\nOption > ")
+        if not op.isdigit() and int(op) in [0,1,2,3,4,5,6]:
+            print "Opcion invalida"
+            continue
+        op = int(op)
+        if op == 0:
+            # Exit
+            break
+        else:
+            # Read the parameters
+            parameter = parameters[op-1]
+            name = parameters[op-1][0].split("/")[-1]
+            corpus = CorpusReader(
+                parameter[0],
+                parameter[1],
+                parameter[2],
+                parameter[3],
+                parameter[4],
+                category_position=parameter[5],
+                category_level=parameter[6],
+                start=parameter[7],
+                decoding=parameter[8],
+            )
+            
+            # Ask how many review for annotating
+            op = ""
+            while not op.isdigit():
+                op = raw_input("\nHow many? > ")
+            left = int(op)
+            
+            # Get the reviews an shuffle them for pick random reviews
+            reviews = list(enumerate(corpus.get_opinions()))
+            random.shuffle(reviews)
+            for id,review in reviews:
+                words = review.split(' ')
+                total = len(words)
+                cats = ['' for _ in range(total)]
+                
+                # For each word annotate with (N) or (I) and give the possibility of back by pressing (B)
+                idx = 0
+                while idx != total:
+                    os.system('clear') 
+                    DisplayStatus(idx,total,words,cats)
+                    cat = raw_input("N(ormal), I(nverted) or B(ack)? > ")
+                    cat = cat.upper()
+                    if not cat or cat not in 'NIBnib':
+                        print "Option",cat,"is not correct." ;raw_input()
+                        continue
+                    if cat == 'B':
+                        # Back
+                        idx = idx - 1 if idx != 0 else 0
+                        cats[idx] = ''
+                    else:
+                        # Associate the category
+                        cats[idx] = cat
+                        idx = idx + 1
+                        
+                # Save the result as two list: words and its respective category for each one 
+                result.append({
+                    "id" : id+1,
+                    "from" : name,
+                    "annotation" : ' '.join(word.lower()+"/"+cat for word,cat in zip(words,cats))
+                })
+                
+                # Check end condition
+                left -= 1
+                if left == 0:
+                    ViewSave(result)
+                    break
+                print "Result added. Are you ready for the next? (left %i)" % left   
+
+# Call to main program
+Main()                        
             
