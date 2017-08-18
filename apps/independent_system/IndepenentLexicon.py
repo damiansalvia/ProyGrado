a82ecb64a592c459322af3df9a77c4b2828b5d90
Damian Salvia <damian.salvia@gmail.com>
2017-08-18 20:06:07 -0300

    Script for transform manual json tagged to database added

--- apps/independent_system/IndepenentLexicon.py
+++ apps/independent_system/IndepenentLexicon.py
@@ -17,8 +17,8 @@ parameters = [
         "\"(.*?)\"[,(?:\\r\\n)]",
         "(neg|pos)/",
         {
-            'neg': 0,
-            'pos': 100
+            'neg': 20,
+            'pos': 80
         },
         "PATH",
         None,
@@ -26,132 +26,132 @@ parameters = [
         0,
         'utf8'
     ),
-#     (
-#         "../../corpus/corpus_cine",
-#         "*.xml",
-#         "<body>(.*?)</body>",
-#         "rank=\"([1-5])\"",
-#         {
-#             '1': 0, 
-#             '2': 25, 
-#             '3': 50, 
-#             '4': 75, 
-#             '5': 100
-#         },
-#         "FILE",
-#         "BEFORE",
-#         None,
-#         0,
-#         'utf8'
-#     ),
-#     (
-#         "../../corpus/corpus_hoteles",
-#         "*.xml",
-#         "<coah:review>(.*?)</coah:review>",
-#         "<coah:rank>([1-5])</coah:rank>",
-#         {
-#             '1': 0, 
-#             '2': 25, 
-#             '3': 50, 
-#             '4': 75, 
-#             '5': 100
-#         },
-#         "FILE",
-#         "BEFORE",
-#         None,
-#         0,
-#         'utf8'
-#     ),
-#     (
-#         "../../corpus/corpus_prensa_uy",
-#         "*.csv",
-#         "\#(.*(?!TRUE|FALSE)),[^ ]", # No considera el test.csv, el "\#" significa "no uses re.DOTALL" 
-#         ",(Neg|Neu|Pos)\\n",
-#         {
-#             'Neg': 0, 
-#             'Neu': 50, 
-#             'Pos': 100
-#         },
-#         "FILE",
-#         "AFTER",
-#         None,
-#         0,
-#         'utf8'
-#     ),
-#     (
-#         "../../corpus/corpus_tweets",
-#         "*.tsv",
-#         "(.*?)\\t.*?\\n",
-#         "(\d\\t\d)\\t",
-#         {
-#             '3\t1': 10, 
-#             '3\t2': 20, 
-#             '2\t4': 90, 
-#             '2\t2': 70, 
-#             '2\t3': 60, 
-#             '4\t2': 30,
-#             '2\t1': 80, 
-#             '5\t1': 40, 
-#             '1\t5': 50, 
-#             '1\t4': 30, 
-#             '4\t1': 50, 
-#             '1\t1': 40, 
-#             '1\t3': 60, 
-#             '1\t2': 70
-#         },
-#         "FILE",
-#         "BEFORE",
-#         None,
-#         1,
-#         'utf8'
-#     ),
-#     (
-#         "../../corpus/corpus_variado_sfu", 
-#         "*/*.txt", 
-#         "(.*)\s",
-#         "(yes|no)_",
-#         {
-#             'no' : 0, 
-#             'yes': 100
-#         },
-#         "PATH",
-#         None,
-#         1,
-#         0,
-#         'utf8'
-#     ),
-#     (
-#         "../../corpus/corpus_tweets_2",
-#         "*.csv",
-#         "\"(.*?)\",",
-#         "(N|P|NEU|NONE)\\n",
-#         {
-#             'N'   :0,
-#             'NEU' :50,
-#             'NONE':50,
-#             'P'   :100
-#         },
-#         "FILE",
-#         "AFTER",
-#         None,
-#         1,
-#         'utf8'
-#     ),
-#     (
-#         "../../corpus/corpus_restaurantes",
-#         "*/*.json",
-#         "\"(.*?)\"[,(?:\\r\\n)]",
-#         "(neg|pos)/",
-#         {
-#             'neg': 0,
-#             'pos': 100
-#         },
-#         "PATH",
-#         None,
-#         0,
-#         0,
-#         'unicode-escape'
-#     )
+    (
+        "../../corpus/corpus_cine",
+        "*.xml",
+        "<body>(.*?)</body>",
+        "rank=\"([1-5])\"",
+        {
+            '1': 0, 
+            '2': 25, 
+            '3': 50, 
+            '4': 75, 
+            '5': 100
+        },
+        "FILE",
+        "BEFORE",
+        None,
+        0,
+        'utf8'
+    ),
+    (
+        "../../corpus/corpus_hoteles",
+        "*.xml",
+        "<coah:review>(.*?)</coah:review>",
+        "<coah:rank>([1-5])</coah:rank>",
+        {
+            '1': 0, 
+            '2': 25, 
+            '3': 50, 
+            '4': 75, 
+            '5': 100
+        },
+        "FILE",
+        "BEFORE",
+        None,
+        0,
+        'utf8'
+    ),
+    (
+        "../../corpus/corpus_prensa_uy",
+        "*.csv",
+        "\#(.*(?!TRUE|FALSE)),[^ ]", # No considera el test.csv, el "\#" significa "no uses re.DOTALL" 
+        ",(Neg|Neu|Pos)\\n",
+        {
+            'Neg': 0, 
+            'Neu': 50, 
+            'Pos': 100
+        },
+        "FILE",
+        "AFTER",
+        None,
+        0,
+        'utf8'
+    ),
+    (
+        "../../corpus/corpus_tweets",
+        "*.tsv",
+        "(.*?)\\t.*?\\n",
+        "(\d\\t\d)\\t",
+        {
+            '3\t1': 10, 
+            '3\t2': 20, 
+            '2\t4': 90, 
+            '2\t2': 70, 
+            '2\t3': 60, 
+            '4\t2': 30,
+            '2\t1': 80, 
+            '5\t1': 40, 
+            '1\t5': 50, 
+            '1\t4': 30, 
+            '4\t1': 50, 
+            '1\t1': 40, 
+            '1\t3': 60, 
+            '1\t2': 70
+        },
+        "FILE",
+        "BEFORE",
+        None,
+        1,
+        'utf8'
+    ),
+    (
+        "../../corpus/corpus_variado_sfu", 
+        "*/*.txt", 
+        "(.*)\s",
+        "(yes|no)_",
+        {
+            'no' : 0, 
+            'yes': 100
+        },
+        "PATH",
+        None,
+        1,
+        0,
+        'utf8'
+    ),
+    (
+        "../../corpus/corpus_tweets_2",
+        "*.csv",
+        "\"(.*?)\",",
+        "(N|P|NEU|NONE)\\n",
+        {
+            'N'   :0,
+            'NEU' :50,
+            'NONE':50,
+            'P'   :100
+        },
+        "FILE",
+        "AFTER",
+        None,
+        1,
+        'utf8'
+    ),
+    (
+        "../../corpus/corpus_restaurantes",
+        "*/*.json",
+        "\"(.*?)\"[,(?:\\r\\n)]",
+        "(neg|pos)/",
+        {
+            'neg': 20,
+            'pos': 80
+        }, 
+        "PATH",
+        None,
+        0,
+        0,
+        'unicode-escape'
+    )
 ] 
 
 count = 0
