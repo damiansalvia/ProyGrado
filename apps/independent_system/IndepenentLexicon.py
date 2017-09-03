32ba5defbc100379d1102ccec260a626f0226a50
Damian Salvia <damian.salvia@gmail.com>
2017-09-03 17:59:43 -0300

    Settings section splitted from main flow

--- apps/independent_system/IndepenentLexicon.py
+++ apps/independent_system/IndepenentLexicon.py
@@ -1,179 +1,40 @@
- # -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
 import sys
 sys.path.append('../utilities')
 from utilities import *
 
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import GridSearchCV
+
 import CorpusReader as cr 
 import DataProvider as dp
 import NegationTagger as nt
 import TextAnalyzer as ta        
+from Settings import *
 
+#################################################################
 
-parameters = [
-    (
-        "../../corpus/corpus_apps_android",
-        "*/*.json",
-        "\"(.*?)\"[,(?:\\r\\n)]",
-        "(neg|pos)/",
-        {
-            'neg': 20,
-            'pos': 80
-        },
-        "PATH",
-        None,
-        0,
-        0,
-        'unicode-escape'
-    ),
-    (
-        "../../corpus/corpus_cine",
-        "*.xml",
-        "<body>(.*?)</body>",
-        "rank=\"([1-5])\"",
-        {
-            '1': 0, 
-            '2': 25, 
-            '3': 50, 
-            '4': 75, 
-            '5': 100
-        },
-        "FILE",
-        "BEFORE",
-        None,
-        0,
-        'cp1252'
-    ),
-    (
-        "../../corpus/corpus_hoteles",
-        "*.xml",
-        "<coah:review>(.*?)</coah:review>",
-        "<coah:rank>([1-5])</coah:rank>",
-        {
-            '1': 0, 
-            '2': 25, 
-            '3': 50, 
-            '4': 75, 
-            '5': 100
-        },
-        "FILE",
-        "BEFORE",
-        None,
-        0,
-        'utf8'
-    ),
-    (
-        "../../corpus/corpus_prensa_uy",
-        "*.csv",
-        "\#(.*(?!TRUE|FALSE)),[^ ]", # No considera el test.csv, el "\#" significa "no uses re.DOTALL" 
-        ",(Neg|Neu|Pos)\\n",
-        {
-            'Neg': 0, 
-            'Neu': 50, 
-            'Pos': 100
-        },
-        "FILE",
-        "AFTER",
-        None,
-        0,
-        'utf8'
-    ),
-    (
-        "../../corpus/corpus_tweets",
-        "*.tsv",
-        "(.*?)\\t.*?\\n",
-        "(\d\\t\d)\\t",
-        {
-            '3\t1': 10, 
-            '3\t2': 20, 
-            '2\t4': 90, 
-            '2\t2': 70, 
-            '2\t3': 60, 
-            '4\t2': 30,
-            '2\t1': 80, 
-            '5\t1': 40, 
-            '1\t5': 50, 
-            '1\t4': 30, 
-            '4\t1': 50, 
-            '1\t1': 40, 
-            '1\t3': 60, 
-            '1\t2': 70
-        },
-        "FILE",
-        "BEFORE",
-        None,
-        1,
-        'utf8'
-    ),
-    (
-        "../../corpus/corpus_variado_sfu", 
-        "*/*.txt", 
-        "(.*)\s",
-        "(yes|no)_",
-        {
-            'no' : 20, 
-            'yes': 80
-        },
-        "PATH",
-        None,
-        1,
-        0,
-        'unicode-escape'
-    ),
-    (
-        "../../corpus/corpus_tweets_2",
-        "*.csv",
-        "\"(.*?)\",",
-        "(N|P|NEU|NONE)\\n",
-        {
-            'N'   :0,
-            'NEU' :50,
-            'NONE':50,
-            'P'   :100
-        },
-        "FILE",
-        "AFTER",
-        None,
-        1,
-        'utf8'
-    ),
-    (
-        "../../corpus/corpus_restaurantes",
-        "*/*.json",
-        "\"(.*?)\"[,(?:\\r\\n)]",
-        "(neg|pos)/",
-        {
-            'neg': 20,
-            'pos': 80
-        }, 
-        "PATH",
-        None,
-        0,
-        0,
-        'unicode-escape'
-    )
-] 
-
-###################################################################
+config_set = config_set_parsing()
 
 op = raw_input("Parse a(ll) or enter for none > ")
-op = len(parameters) if op.lower() == 'a' else 0
+op = len(config_set) if op.lower() == 'a' else 0
 
 count = 0
-for parameter in parameters[:op]: 
-                  
+for config in config_set[:op]:
+        
     opinions = cr.from_corpus(
-            parameter[0], # source
-            parameter[1], # path pattern to file
-            parameter[2], # review pattern
-            parameter[3], # category pattern
-            parameter[4], # category mapping
-            parameter[5], # category mapping
-            category_position=parameter[6],
-            category_level=parameter[7],
-            start=parameter[8],
-            decoding=parameter[9],
-            tofile="./outputs/tmp"
+            config['source'],            
+            config['path_pattern'],      
+            config['review_pattern'],    
+            config['category_pattern'],  
+            config['category_mapping'],  
+            config['category_location'], 
+            category_position = config['category_position'],
+            category_level    = config['category_level'],
+            start             = config['start'],
+            decoding          = config['decoding'],
+            tofile = "./outputs/tmp"
         )
                
     analyzed = ta.analyze(
@@ -198,16 +59,30 @@ op = op.lower()
 if op == 'y': nt.start_tagging(tofile="./outputs/negation") 
 
 ###################################################################
-
-config = {
-    "left" :3,
-    "right":1
-}
   
-ann = nt.NeuralNegationTagger( config['left'] , config['right'] )     
-ann.fit_tagged( testing_fraction=0.20 , verbose=1 )
-# ann.save()
-ann.predict_untagged( tofile="./outputs/tmp" )    
+config_set = config_set_neural_negation_tagger()
+
+op = raw_input("Traing-predict a(ll) or enter for none > ")
+op = len(config_set) if op.lower() == 'a' else 0
+
+stats = []
+for config in config_set[:op]:
+    ann = nt.NeuralNegationTagger( 
+        config['wleft'], 
+        config['wright'],
+        out_dims      = config['out_dims'],
+        activation    = config['activation'], 
+        loss          = config['loss'],
+        optimizer     = config['optimizer'], 
+        early_monitor = config['early_monitor'], 
+        drop_rate     = config['drop_rate'] 
+    )     
+    stat = ann.fit_tagged( testing_fraction=0.20 , verbose=1 )
+    stats.append(stat)
+    ann.predict_untagged( limit = 10, tofile="./outputs/tmp" )
+
+print "Summary"
+for nth,stat in enumerate(stats): print "Option",nt,stat
 
 ###################################################################
 
---
+++ apps/independent_system/Settings.py
@@ -0,0 +1,253 @@
+# -*- coding: utf-8 -*-
+
+def config_set_parsing():
+    return [
+        {
+            'source'           :"../../corpus/corpus_apps_android",
+            'path_pattern'     :"*/*.json",
+            'review_pattern'   :"\"(.*?)\"[,(?:\\r\\n)]",
+            'category_pattern' :"(neg|pos)/",
+            'category_mapping' :{
+                'neg': 20,
+                'pos': 80
+            },
+            'category_location':"PATH",
+            'category_position':None,
+            'category_level'   :0,
+            'start'            :0,
+            'decoding'         :'unicode-escape'
+        },
+        {
+            'source'            :"../../corpus/corpus_cine",
+            'path_pattern'      :"*.xml",
+            'review_pattern'    :"<body>(.*?)</body>",
+            'category_pattern'  :"rank=\"([1-5])\"",
+            'category_mapping'  :{
+                '1': 0, 
+                '2': 25, 
+                '3': 50, 
+                '4': 75, 
+                '5': 100
+            },
+            'category_location' :"FILE",
+            'category_position' :"BEFORE",
+            'category_level'    :None,
+            'start'             :0,
+            'decoding'          :'cp1252'
+        },
+        {
+            'source'            :"../../corpus/corpus_hoteles",
+            'path_pattern'      :"*.xml",
+            'review_pattern'    :"<coah:review>(.*?)</coah:review>",
+            'category_pattern'  :"<coah:rank>([1-5])</coah:rank>",
+            'category_mapping'  :{
+                '1': 0, 
+                '2': 25, 
+                '3': 50, 
+                '4': 75, 
+                '5': 100
+            },
+            'category_location' :"FILE",
+            'category_position' :"BEFORE",
+            'category_level'    :None,
+            'start'             :0,
+            'decoding'          :'utf8'
+        },
+        {
+            'source'            :"../../corpus/corpus_prensa_uy",
+            'path_pattern'      :"*.csv",
+            'review_pattern'    :"\#(.*(?!TRUE|FALSE)),[^ ]", # No considera el test.csv, el "\#" significa "no uses re.DOTALL" 
+            'category_pattern'  :",(Neg|Neu|Pos)\\n",
+            'category_mapping'  :{
+                'Neg': 0, 
+                'Neu': 50, 
+                'Pos': 100
+            },
+            'category_location' :"FILE",
+            'category_position' :"AFTER",
+            'category_level'    :None,
+            'start'             :0,
+            'decoding'          :'utf8'
+        },
+        {
+            'source'            :"../../corpus/corpus_tweets",
+            'path_pattern'      :"*.tsv",
+            'review_pattern'    :"(.*?)\\t.*?\\n",
+            'category_pattern'  :"(\d\\t\d)\\t",
+            'category_mapping'  :{
+                '3\t1': 10, 
+                '3\t2': 20, 
+                '2\t4': 90, 
+                '2\t2': 70, 
+                '2\t3': 60, 
+                '4\t2': 30,
+                '2\t1': 80, 
+                '5\t1': 40, 
+                '1\t5': 50, 
+                '1\t4': 30, 
+                '4\t1': 50, 
+                '1\t1': 40, 
+                '1\t3': 60, 
+                '1\t2': 70
+            },
+            'category_location' :"FILE",
+            'category_position' :"BEFORE",
+            'category_level'    :None,
+            'start'             :1,
+            'decoding'          :'utf8'
+        },
+        {
+            'source'            :"../../corpus/corpus_variado_sfu", 
+            'path_pattern'      :"*/*.txt", 
+            'review_pattern'    :"(.*)\s",
+            'category_pattern'  :"(yes|no)_",
+            'category_mapping'  :{
+                'no' : 20, 
+                'yes': 80
+            },
+            'category_location' :"PATH",
+            'category_position' :None,
+            'category_level'    :1,
+            'start'             :0,
+            'decoding'          :'unicode-escape'
+        },
+        {
+            'source'            :"../../corpus/corpus_tweets_2",
+            'path_pattern'      :"*.csv",
+            'review_pattern'    :"\"(.*?)\",",
+            'category_pattern'  :"(N|P|NEU|NONE)\\n",
+            'category_mapping'  :{
+                'N'   :0,
+                'NEU' :50,
+                'NONE':50,
+                'P'   :100
+            },
+            'category_location' :"FILE",
+            'category_position' :"AFTER",
+            'category_level'    :None,
+            'start'             :1,
+            'decoding'          :'utf8'
+        },
+        {
+            'source'            :"../../corpus/corpus_restaurantes",
+            'path_pattern'      :"*/*.json",
+            'review_pattern'    :"\"(.*?)\"[,(?:\\r\\n)]",
+            'category_pattern'  :"(neg|pos)/",
+            'category_mapping'  :{
+                'neg': 20,
+                'pos': 80
+            }, 
+            'category_location' :"PATH",
+            'category_position' :None,
+            'category_level'    :0,
+            'start': 0,
+            'decoding'          :'unicode-escape'
+        }
+] 
+    
+def config_set_neural_negation_tagger():
+    # Activations: relu [max(0,x)], linear [x], tanh, softplus [ln(1+exp(x)], selu [x:x>0;aexp(x)-a:x<=0], sigmoid [1/(1+exp(-x))]
+    # Optimizers:  adam, sgd, rmsprop
+    # Monitors: None, val_binary_accuracy o val_precision
+    return [
+        {
+            'wleft'         :2,
+            'wright'        :2,
+            'out_dims'      :[750],
+            'activation'    :['relu','sigmoid'],
+            'loss'          :'binary_crossentropy',
+            'optimizer'     :'adam',
+            'early_monitor' :'val_binary_accuracy',
+            'drop_rate'     :[0.2]
+        },
+        {
+            'wleft'         :3,
+            'wright'        :3,
+            'out_dims'      :[1200],
+            'activation'    :['relu','sigmoid'],
+            'loss'          :'binary_crossentropy',
+            'optimizer'     :'adam',
+            'early_monitor' :'val_binary_accuracy',
+            'drop_rate'     :[0.2]
+        },
+        {
+            'wleft'         :4,
+            'wright'        :2,
+            'out_dims'      :[1200],
+            'activation'    :['relu','sigmoid'],
+            'loss'          :'binary_crossentropy',
+            'optimizer'     :'adam',
+            'early_monitor' :'val_binary_accuracy',
+            'drop_rate'     :[0.2]
+        },
+        {
+            'wleft'         :4,
+            'wright'        :3,
+            'out_dims'      :[1300],
+            'activation'    :['relu','sigmoid'],
+            'loss'          :'binary_crossentropy',
+            'optimizer'     :'adam',
+            'early_monitor' :'val_binary_accuracy',
+            'drop_rate'     :[0.2]
+        },
+        {
+            'wleft'         :2,
+            'wright'        :2,
+            'out_dims'      :[750,500],
+            'activation'    :['relu','relu','sigmoid'],
+            'loss'          :'binary_crossentropy',
+            'optimizer'     :'adam',
+            'early_monitor' :'val_binary_accuracy',
+            'drop_rate'     :[0.2,0.2]
+        },
+        {
+            'wleft'         :3,
+            'wright'        :3,
+            'out_dims'      :[1200,700],
+            'activation'    :['relu','relu','sigmoid'],
+            'loss'          :'binary_crossentropy',
+            'optimizer'     :'adam',
+            'early_monitor' :'val_binary_accuracy',
+            'drop_rate'     :[0.2,0.2]
+        },
+        {
+            'wleft'         :4,
+            'wright'        :2,
+            'out_dims'      :[1200,700],
+            'activation'    :['relu','relu','sigmoid'],
+            'loss'          :'binary_crossentropy',
+            'optimizer'     :'adam',
+            'early_monitor' :'val_binary_accuracy',
+            'drop_rate'     :[0.2,0.2]
+        },
+        {
+            'wleft'         :4,
+            'wright'        :3,
+            'out_dims'      :[1300,800],
+            'activation'    :['relu','relu','sigmoid'],
+            'loss'          :'binary_crossentropy',
+            'optimizer'     :'adam',
+            'early_monitor' :'val_binary_accuracy',
+            'drop_rate'     :[0.2,0.2]
+        },
+        {
+            'wleft'         :2,
+            'wright'        :2,
+            'out_dims'      :[750,500],
+            'activation'    :['selu','selu','sigmoid'],
+            'loss'          :'binary_crossentropy',
+            'optimizer'     :'adam',
+            'early_monitor' :'val_binary_accuracy',
+            'drop_rate'     :[0.2,0.2]
+        },
+        {
+            'wleft'         :3,
+            'wright'        :3,
+            'out_dims'      :[1200,700],
+            'activation'    :['selu','selu','sigmoid'],
+            'loss'          :'binary_crossentropy',
+            'optimizer'     :'adam',
+            'early_monitor' :'val_binary_accuracy',
+            'drop_rate'     :[0.2,0.2]
+        },
+    ]
