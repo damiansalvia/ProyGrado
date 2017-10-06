e54f05b82dc6e6c4334fb4692e1be0f250dd0e80
Damian Salvia <damian.salvia@gmail.com>
2017-10-05 22:49:04 -0300

    Text correction mechanism added

--- apps/utilities/analyzer.py
+++ apps/utilities/analyzer.py
@@ -1,23 +1,32 @@
 # -*- encoding: utf-8 -*-
 import freeling 
+import os
+from enchant import DictWithPWL
+from enchant.checker import SpellChecker
+from difflib import get_close_matches
+
+
+DATA = "/usr/local/share/freeling/"
+LANG="es"
+
+assert os.path.getsize('../utilities/es-lat') > 0
+my_dict = DictWithPWL('es', '../utilities/es-lat')
+assert my_dict.provider.name == 'aspell'
+chkr = SpellChecker(my_dict)
+
 
 class Analyzer:
 
     def __init__(self):
-        DATA = "/usr/local/share/freeling/";
-        LANG="es";
 
         freeling.util_init_locale("default")
 
-        # Create language analyzer
-#         la=freeling.lang_ident(DATA+"common/lang_ident/ident.dat")
-
         # Create options set for maco analyzer
-        op = freeling.maco_options("es")
+        op = freeling.maco_options(LANG)
         op.set_data_files( 
             "", 
             DATA + "common/punct.dat",
-            DATA + LANG + "/dicc.src",
+            DATA + LANG + "/es-ar/dicc.src",
             DATA + LANG + "/afixos.dat",
             "",
             DATA + LANG + "/locucions.dat", 
@@ -31,7 +40,22 @@ class Analyzer:
         self.sp  = freeling.splitter(DATA+LANG+"/splitter.dat")
         self.mf  = freeling.maco(op)
 
-        # Activate morpho modules to be used in next call
+        # create tagger and alternatives
+        self.tg  = freeling.hmm_tagger(DATA+LANG+"/tagger.dat",True,2)
+        self.alts_ort = freeling.alternatives(DATA+LANG+"/alternatives-ort.dat")
+        self.alts_phon = freeling.alternatives(DATA+LANG+"/alternatives-phon.dat")
+        
+        self.sid = self.sp.open_session()
+        
+    def __del__(self):
+        self.sp.close_session(self.sid)
+
+
+    def analyze(self,text):
+        if not isinstance(text, unicode):
+            text = unicode(text, 'utf8')
+        
+        # Correction from alternatives
         self.mf.set_active_options(
             False, # User Map
             False,  # Number Detection 
@@ -44,59 +68,97 @@ class Analyzer:
             False,  # Multiword Detection 
             True,  # NER 
             False,  # Quantities Detection
-            True   # Probability Assignment
+            False   # Probability Assignment <-- Required
         )
-
-        # create tagger, sense anotator, and parsers
-        self.tg  = freeling.hmm_tagger(DATA+LANG+"/tagger.dat",True,2)
-#         self.sen = freeling.senses(DATA+LANG+"/senses.dat")
-#         parser= freeling.chart_parser(DATA+LANG+"/chunker/grammar-chunk.dat")
-#         self.dep=freeling.dep_txala(DATA+LANG+"/dep_txala/dependences.dat", parser.get_start_symbol())
-
-
-    def analyze(self,text):
-        if not isinstance(text, unicode):
-            text = unicode(text, 'utf8')
-            
-        sid = self.sp.open_session()
-            
-        l  = self.tk.tokenize(text)
-        ls = self.sp.split(sid,l,False)
+        ls = self.tk.tokenize(text)
+        ls = self.sp.split(self.sid,ls,False)
+        ls = self.mf.analyze(ls)
+        ls = self.alts_ort.analyze(ls)
+#         ls = self.alts_phon.analyze(ls)
+        unk = [] ; text = ""
+        for sentence in ls :
+            for token in sentence.get_words() :
+                word  = token.get_lc_form()
+                known = token.found_in_dict()
+                for wd in word.split('_'):
+                    if not known:
+                        unk.append( wd )
+                    text += ' '+ wd
+                
+#                 if raw_input(token.get_lc_form()+' > ') == 'd':
+#                     import pdb;pdb.set_trace()           
+        if unk: # Do word correction
+            chkr.set_text(text)
+            for err in chkr:
+                sugg = [ s for s in chkr.suggest(err.word) if '-' not in s ]
+                alts = get_close_matches( err.word , sugg , 3 , 0.80 )
+                if alts   : corr = alts[0]
+                elif sugg : corr = sugg[0]
+                else      : corr = err.word
+                if err.word in unk: # Avoid correct nouns
+                    err.replace(corr)
+            text = chkr.get_text()
+            unk = []
+        
+        # Analyze result
+        self.mf.set_active_options(
+            False, # User Map
+            False,  # Number Detection 
+            True,  # Punctuation Detection 
+            False,  # Date Detection 
+            True,  # Dictionary Search 
+            True,  # Affix Analysis
+            False, # Compound Analysis
+            True,  # Retok Contractions
+            False,  # Multiword Detection 
+            True,  # NER 
+            False,  # Quantities Detection
+            True   # Probability Assignment <-- Required
+        )
+        ls = self.tk.tokenize(text)
+        ls = self.sp.split(self.sid,ls,False)
         ls = self.mf.analyze(ls)
         ls = self.tg.analyze(ls)
-
-        ## output results
         sent = []
-        for s in ls :
-            ws = s.get_words()
-            for w in ws :
+        for sentence in ls :
+            for token in sentence.get_words() :
                 sent.append({
-                    "form"  : w.get_form(),
-                    "lemma" : w.get_lemma(), 
-                    "tag"   : w.get_tag()
+                    "form"  : token.get_lc_form(),
+                    "lemma" : token.get_lemma(), 
+                    "tag"   : token.get_tag()
                 })
-
-        self.sp.close_session(sid)
+                
+#         self.sp.close_session(sid)
         return sent
+
 
 #--------------- EXAMPLE ---------------
 
 if __name__ == "__main__":
 
     analiyzer = Analyzer()
-    #------------------------------------------------------
-    print "\n   Unfinished phrase"
-    print analiyzer.analyze("Esto es una prueba")
-    #------------------------------------------------------
-    print "\n   Complete phrase"
-    print analiyzer.analyze("Esto es una prueba.")
-    #------------------------------------------------------
-    print "\n   Exclamation phrase"
-    print analiyzer.analyze("Esto es una prueba!")
-    #------------------------------------------------------
-    print "\n   Question phrase"
-    print analiyzer.analyze("¿Será esto una prueba?")
-    #------------------------------------------------------
-    print "\n   Phrases with special characters"
-    print analiyzer.analyze("Mañana haré pruebas con pingüinos.")
+    while True:
+        text = raw_input('> ')
+        if not text: break
+        result =  analiyzer.analyze(text)
+        for res in result: print res
+#     #------------------------------------------------------
+#     print "\n   With unkown words"
+#     result =  analiyzer.analyze("Horrible Se cuelga. Tengo Nexus y Maria Soledad Garcia ttienne un Google que es buenisimo jajaja. Queres ir al 23 a haser ALGO o no quereis xq es mini jeje. Revis mi mail.")
+#     for res in result: print res
+#     #------------------------------------------------------
+#     print "\n   Unfinished phrase"
+#     print analiyzer.analyze("Esto es una prueba")
+#     #------------------------------------------------------
+#     print "\n   Complete phrase"
+#     print analiyzer.analyze("Esto es una prueba.")
+#     #------------------------------------------------------
+#     print "\n   Exclamation phrase"
+#     print analiyzer.analyze("Esto es una prueba!")
+#     #------------------------------------------------------
+#     print "\n   Question phrase"
+#     print analiyzer.analyze("¿Será esto una prueba?")
+#     #------------------------------------------------------
+#     print "\n   Phrases with special characters"
+#     print analiyzer.analyze("Mañana haré pruebas con pingüinos.")
 
