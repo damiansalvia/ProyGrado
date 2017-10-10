1648492b57ba0fca8ca033e91823f290d80662d6
Damian Salvia <damian.salvia@gmail.com>
2017-10-09 22:43:39 -0300

    package done

--- src/cldas/__init__.py
+++ src/cldas/__init__.py
@@ -1,4 +1,6 @@
 from cldas.retrieval import CorpusReader
 from cldas.morpho import Preprocess
+from cldas.indeplex import by_senti_avg, by_senti_pmi, by_senti_qtf, by_senti_tfidf
+from cldas.deplex import by_bfs, by_dijkstra
 
 __all__ = ['utils','neg']
