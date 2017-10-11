e2d78efed18189abcbbc696a6f56df5dc1ba0cd2
Damian Salvia <damian.salvia@gmail.com>
2017-10-11 17:48:43 -0300

    improve of blocking logfile

--- src/cldas/utils/logger.py
+++ src/cldas/utils/logger.py
@@ -13,11 +13,9 @@ class Log:
     def __init__(self,logdir,logname="clds.log"):
         logdir = logdir if logdir[-1] != "/" else logdir[:-1]
         if not os.path.isdir(logdir): os.makedirs(logdir)
-        self.log = open(logdir+"/"+logname, 'a')
+        self.logpath = logdir+"/"+logname
 
     def __call__(self,message, level='error'):
-        time = strftime("%Y-%m-%d %H:%M:%S", gmtime())
-        self.log.write( "\n%s : %s at %s > %s \n" % ( level.upper() , time , inspect.stack()[1][0].f_code.co_name.upper() , message ) )
-        
-    def __exit__(self):
-        self.log.close()
+        with open(self.logpath, 'a') as log:
+            time = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+            log.write( "\n%s : %s at %s > %s \n" % ( level.upper() , time , inspect.stack()[1][0].f_code.co_name.upper() , message ) )
