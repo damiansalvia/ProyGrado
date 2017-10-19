d07eb2924f1710fd03e3ca98d8cd2f663cea2d61
Damian Salvia <damian.salvia@gmail.com>
2017-10-19 16:10:54 -0300

    minor change

--- src/cldas/utils/__init__.py
+++ src/cldas/utils/__init__.py
@@ -4,4 +4,12 @@ from cldas.utils.visual import progress, title
 from cldas.utils import metrics
 from cldas.utils.graph import MultiGraph
 
-USEFUL_TAGS = ['AQ','RG','DD','NC','VM','PD','PI']
+USEFUL_TAGS = [
+    'AQ', # Adjetivo calificativos     - p.e. alegre   
+    'RG', # Adverbio General           - p.e. despacio 
+    'DD', # Determinante Demostrativo  - p.e. este     
+    'NC', # Nombre Comun               - p.e. gato     
+    'VM', # Verbo Principal            - p.e. Pedro    
+    'PD', # Pronombre Demostrativo     - p.e. aquel    
+    'PI'  # Pronombre Indefinido       - p.e. bastante 
+]
