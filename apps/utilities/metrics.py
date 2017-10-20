43dda690d13b0bbd5ff3a7717baf1f21e6b96655
nicoMechu <nicomebu@gmail.com>
2017-10-20 20:51:55 -0300

    start evaluation

--- apps/utilities/metrics.py
+++ apps/utilities/metrics.py
@@ -1,6 +1,5 @@
-import numpy as np
 import keras.backend as K
-
+import sklearn.metrics as sk_metrics
 
 def binary_accuracy(y_true, y_pred):
     return K.mean(K.equal(y_true, K.round(y_pred)), axis=-1)
@@ -24,21 +23,25 @@ def precision(y_true, y_pred):
     '''Calculates the precision, a metric for multi-label classification of
     how many selected items are relevant.
     '''
-    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
-    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
-    precision = true_positives / (predicted_positives + K.epsilon())
-    return precision
-
+    if type(y_pred).__name__ == 'Variable':
+        true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+        predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+        precision = true_positives / (predicted_positives + K.epsilon())
+        return precision
+    else:
+        return sk_metrics.precision_score(y_true, y_pred)
 
 def recall(y_true, y_pred):
     '''Calculates the recall, a metric for multi-label classification of
     how many relevant items are selected.
     '''
-    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
-    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
-    recall = true_positives / (possible_positives + K.epsilon())
-    return recall
-
+    if type(y_pred).__name__ == 'Variable':
+        true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+        possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+        recall = true_positives / (possible_positives + K.epsilon())
+        return recall
+    else:
+        return sk_metrics.recall_score(y_true, y_pred)   
 
 def fbeta_score(y_true, y_pred, beta=1):
     '''Calculates the F score, the weighted harmonic mean of precision and recall.
@@ -53,18 +56,21 @@ def fbeta_score(y_true, y_pred, beta=1):
     correct classes becomes more important, and with beta > 1 the metric is
     instead weighted towards penalizing incorrect class assignments.
     '''
-    if beta < 0:
-        raise ValueError('The lowest choosable beta is zero (only precision).')
-        
-    # If there are no true positives, fix the F score at 0 like sklearn.
-    if K.sum(K.round(K.clip(y_true, 0, 1))) == 0:
-        return 0
+    if type(y_pred).__name__ == 'Variable':
+        if beta < 0:
+            raise ValueError('The lowest choosable beta is zero (only precision).')
+            
+        # If there are no true positives, fix the F score at 0 like sklearn.
+        if K.sum(K.round(K.clip(y_true, 0, 1))) == 0:
+            return 0
 
-    p = precision(y_true, y_pred)
-    r = recall(y_true, y_pred)
-    bb = beta ** 2
-    fbeta_score = (1 + bb) * (p * r) / (bb * p + r + K.epsilon())
-    return fbeta_score
+        p = precision(y_true, y_pred)
+        r = recall(y_true, y_pred)
+        bb = beta ** 2
+        fbeta_score = (1 + bb) * (p * r) / (bb * p + r + K.epsilon())
+        return fbeta_score
+    else:
+        return sk_metrics.fbeta_score(y_true, y_pred, beta)  
 
 
 def fmeasure(y_true, y_pred):
