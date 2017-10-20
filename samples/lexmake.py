b80dd864d70e34be064c422df40f4095441dd329
Damian Salvia <damian.salvia@gmail.com>
2017-10-20 00:56:22 -0300

    init commit

---
+++ samples/lexmake.py
@@ -0,0 +1,89 @@
+# -*- encoding: utf-8 -*-
+'''
+Example for generating independent and dependent lexicon from a given corpus by UI
+@author: Nicolás Mechulam, Damián Salvia
+'''
+import sys
+sys.path.append('../src')
+
+import os
+clean = 'cls' if os.name == 'nt' else 'clear'
+
+import glob
+
+import cldas.db.crud as dp
+
+from cldas.indeplex import *
+from cldas.deplex import *
+from cldas.utils import load, title, USEFUL_TAGS
+
+
+def display_options(name, options, files=[]):
+    
+    title(name,30)
+    
+    for i in range( len(options) ):
+        print i,".",options[i] if not callable( options[i] ) else options[i].__name__.replace('_',' ')
+        
+    for j in range( len(files) ):
+        print j+i+1,".",files[j]
+        
+    op = ''
+    while not op.isdigit() or int(op) > len( options+files ):
+        op = raw_input('> ')
+    op = int(op)
+    
+    return op
+
+
+'''
+---------------------------------------------
+      Dataset retrieval (db or custom)
+---------------------------------------------
+'''
+pos = dp.get_opinions( cat_cond={"$gt":50} )
+neg = dp.get_opinions( cat_cond={"$lt":50} )
+lemmas = dp.get_lemmas()
+
+
+'''
+---------------------------------------------
+      Independent Lexicon generation
+---------------------------------------------
+'''
+os.system(clean)
+
+options = [ by_senti_tfidf, by_senti_qtf, by_senti_avg, by_senti_pmi ]
+files   = glob.glob("./indeplex/*")
+i = display_options( "Independent Lexicon", options, files)
+
+limit = len(options)
+if i > limit:
+    li = load( files[i-limit] )
+else:
+    li = options[i]( pos, neg, lemmas, filter_tags=USEFUL_TAGS, limit=150, tofile='./indeplex' )
+
+
+'''
+---------------------------------------------
+      Dependent Lexicon generation
+---------------------------------------------
+'''
+os.system(clean)
+
+corporea = dp.get_sources()
+i = display_options("Sources",corporea)
+corpus   = corporea[i]
+opinions = dp.get_opinions( source=corpus )
+graph = MultiGraph( opinions, corpus, filter_tags=USEFUL_TAGS )
+
+
+os.system(clean)
+
+options = [ by_bfs, by_influence ]
+i = display_options("Dependent Lexicon",options)
+ld = options[i]( graph, li, limit=300, tofile='./deplex' )
+
+
+
+
