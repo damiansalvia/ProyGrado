e2447ce3bbd733b36ad4f69b36a8743f76cb89d6
Damian Salvia <damian.salvia@gmail.com>
2017-10-20 23:51:30 -0300

    positive color update

--- src/cldas/utils/file.py
+++ src/cldas/utils/file.py
@@ -55,7 +55,7 @@ def load(abspath):
  
 
 def _pos_color(word=None, font_size=None, position=None,  orientation=None, font_path=None, random_state=None):
-    h = int(360.0 * 338.0 / 255.0)
+    h = int(360.0 * 136.0 / 255.0)
     s = int(100.0 * 255.0 / 255.0)
     l = int(100.0 * float(random_state.randint(60, 120)) / 255.0)
     return "hsl({}, {}%, {}%)".format(h, s, l)  
