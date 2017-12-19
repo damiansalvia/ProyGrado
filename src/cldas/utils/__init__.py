486a2153eb50cf98250574f08781bb0d07499c9c
Damian Salvia <damian.salvia@gmail.com>
2017-12-19 19:19:54 -0300

    minor change

--- src/cldas/utils/__init__.py
+++ src/cldas/utils/__init__.py
@@ -9,7 +9,7 @@ USEFUL_TAGS = [
     'RG', # Adverbio General           - p.e. despacio 
     'DD', # Determinante Demostrativo  - p.e. este     
     'NC', # Nombre Comun               - p.e. gato     
-    'VM', # Verbo Principal            - p.e. Pedro    
+    'VM', # Verbo Principal            - p.e. comer    
     'PD', # Pronombre Demostrativo     - p.e. aquel    
     'PI'  # Pronombre Indefinido       - p.e. bastante 
 ]
